769ed379cae437f48a16ca34b954db941fa05adf
rmaruyama <r.maruyama@gmail.com>
2022-05-14 06:37:56 -0400

    delete __name__ part

--- num_count.py
+++ num_count.py
@@ -3,8 +3,7 @@
 # 数字でない文字は無視する。
 # 例えば、円周率に現れる数字のばらつきを調べたりできる。
 
-if __name__ == '__main__':
-    counter = [0] * 10
+counter = [0] * 10
 
 with open("num_count_input.txt", mode='r') as f:
     for line in f:  # 1行ずつ
